04e0257ac0ee5e41fbd9d8481c24c0c3aa207b2d
Hitesh Tare <hiteshtare@gmail.com>
2025-12-12 15:33:40 +0530

    Tested Click selector for Sadhana Sangam event

--- tests/english/en.priority-pages.spec.ts
+++ tests/english/en.priority-pages.spec.ts
@@ -18,7 +18,7 @@ test.afterAll(async () => {
   await vrt.stop();
 });
 
-test.describe.skip("English - PriorityPages", () => {
+test.describe("English - PriorityPages", () => {
   trackPagesInVRT(
     vrt,
     trackOptions,
