09edd4c91a8f86180e7c5f64f90ede0d370c2f3b
Hitesh Tare <hiteshtare@gmail.com>
2025-12-23 18:30:26 +0530

    Cleanup

--- tests/english/en.blogs.spec.ts
+++ tests/english/en.blogs.spec.ts
@@ -18,7 +18,7 @@ test.afterAll(async () => {
   await vrt.stop();
 });
 
-test.describe("English - Blogs", () => {
+test.describe.only("English - Blogs", () => {
   trackPagesInVRT(
     vrt,
     trackOptions,
--- tests/english/en.events.spec.ts
+++ tests/english/en.events.spec.ts
@@ -18,7 +18,7 @@ test.afterAll(async () => {
   await vrt.stop();
 });
 
-test.describe.only("English - Events", () => {
+test.describe("English - Events", () => {
   trackPagesInVRT(
     vrt,
     trackOptions,
