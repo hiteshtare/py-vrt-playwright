2bd3188052196b6019b5c98fcdc3692fdeb18582
Hitesh Tare <hiteshtare@gmail.com>
2026-01-16 11:31:00 +0530

    Cleanup 14

--- tests/languages/english/en.image-gallery.spec.ts
+++ tests/languages/english/en.image-gallery.spec.ts
@@ -18,7 +18,7 @@ test.afterAll(async () => {
   await vrt.stop();
 });
 
-test.describe("English - ImageGallery", () => {
+test.describe.only("English - ImageGallery", () => {
   trackPagesInVRT(
     vrt,
     trackOptions,
--- tests/util/regenerate-jsons.ts
+++ tests/util/regenerate-jsons.ts
@@ -56,7 +56,8 @@ export async function regenerateJSONS() {
   // let name = APP_CONFIG.projects.English.Locations.name;
   // let name = APP_CONFIG.projects.English.ImageGallery.name;
   // let name = APP_CONFIG.projects.English.Bookstore.name;
-  let name = APP_CONFIG.projects.English.TeachingsLibrary.name;
+  // let name = APP_CONFIG.projects.English.TeachingsLibrary.name;
+  // let name = APP_CONFIG.projects.English.Templates.name;
 
   try {
     let message = "";
